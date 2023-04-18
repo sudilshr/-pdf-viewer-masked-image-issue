56c18c1cbf0ec310064a8a924335ed12fe01b321
sudilshr <sudilshr@gmail.com>
2023-04-18 13:36:40 +1000

    4 files modified, 9 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, NgxExtendedPdfViewerModule],
+  template: `
+    <h1>PdfViewerMaskedImageIssue</h1>
+    <ngx-extended-pdf-viewer [src]="pdfSrc"> </ngx-extended-pdf-viewer>
+  `,
+})
+export class App {
+  pdfSrc = '/assets/sandwich-masked.pdf';
+}
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(App);
